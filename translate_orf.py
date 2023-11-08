e1b3c785c1313c77dfc5f5e502ee1b400261cebb
Alex Everett <aeverett4746@gmail.com>
2023-11-07 19:17:48 -0700

    Added translate_orf.py for final step of Exercise 8

---
+++ translate_orf.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+import argparse
+from find_orf import find_first_orf
+from find_orf import parse_sequence_from_path
+from translate import translate_sequence
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    default_start_codons = ['AUG']
+    default_stop_codons = ['UAA', 'UAG', 'UGA']
+
+    parser.add_argument('sequence',
+            metavar = 'SEQUENCE',
+            type = str,
+            help = ('The sequence to search for an open-reading frame. '
+                    'If the path flag (\'-p\'/\'--path\') is specified, '
+                    'then this should be a path to a file containing the '
+                    'sequence to be searched.'))
+    parser.add_argument('-p', '--path',
+            action = 'store_true',
+            help = ('The sequence argument should be treated as a path to a '
+                    'containing the sequence to be searched.'))
+    parser.add_argument('-s', '--start-codon',
+            type = str,
+            action = 'append',
+            default = None,
+            help = ('A start codon. This option can be used multiple times '
+                    'if there are multiple start codons. '
+                    'Default: {0}.'.format(" ".join(default_start_codons))))
+    parser.add_argument('-x', '--stop-codon',
+            type = str,
+            action = 'append',
+            default = None,
+            help = ('A stop codon. This option can be used multiple times '
+                    'if there are multiple stop codons. '
+                    'Default: {0}.'.format(" ".join(default_stop_codons))))
+
+    args = parser.parse_args()
+    if args.path:
+        sequence = parse_sequence_from_path(args.sequence)
+    else:
+        sequence = args.sequence
+
+    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+
+    if not args.start_codon:
+        args.start_codon = default_start_codons
+    if not args.stop_codon:
+        args.stop_codon = default_stop_codons
+
+    orf = find_first_orf(sequence = sequence,
+            start_codons = args.start_codon,
+            stop_codons = args.stop_codon)
+    translated_sequence = translate_sequence(orf, genetic_code)
+    return translated_sequence
+
+if __name__=="__main__":
+    print(main())
